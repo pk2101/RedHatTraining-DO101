a2b8b00ba4aac9851ccdc96a78e836ac2f560243
Sushanta Das <susdas@redhat.com>
2021-09-15 10:45:30 +0530

    added /redhat endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World Welcome to Openshift!\n');
 });
 
+app.get('/redhat', function (req, res) {
+  res.send('Welcome to Redhat!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
