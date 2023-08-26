ed7a458b84cc772abb187de64df9fa3baabadad4
Lalana <Lalana_9@hotmail.com>
2023-08-26 10:20:06 +0200

    commit

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World Welcome to Openshift!\n');
 });
 
+app.get('/Mars', function (req, res) {
+  res.send('Hello World Mars!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
